a605b4fc42934ca8c752c27d480c4211b510ff2a
Sockalingam <74052417+Sockalingam29@users.noreply.github.com>
2022-07-29 18:13:56 +0530

    updates for deployment

--- backend/index.js
+++ backend/index.js
@@ -12,12 +12,15 @@ app.use(bodyParser.json({ limit: '200KB', extended: true }))
 app.use(bodyParser.urlencoded({ limit: '200KB', extended: true }))
 app.use(cors());    
 
+
 app.use("/posts",postRoutes);
 
 app.get("/",(req,res)=>{
-    res.send("Hello!");
+    res.send("Hello to memories api");
 });
 
+const PORT = process.env.PORT || 5000;
+
 mongoose.connect(process.env.CONNECTION_URL)
-    .then(()=>app.listen(process.env.PORT || 5000,()=>console.log("Port running")))
+    .then(()=>app.listen(PORT,()=>console.log("Port running")))
     .catch((err)=>console.log(err));
