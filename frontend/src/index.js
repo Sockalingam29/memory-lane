36e3fc46b9e6bc287e34bb8ab457568200ed1e0a
Sockalingam <74052417+Sockalingam29@users.noreply.github.com>
2022-09-03 20:55:48 +0530

    configureStore added

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,16 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import {createStore,applyMiddleware,compose} from 'redux';
-import thunk from 'redux-thunk';
+import configureStore from './configureStore';
 
-import {reducers} from './reducers';
 import './index.css';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-const store = createStore(reducers,compose(applyMiddleware(thunk)));
-
+const store=configureStore();
 
 ReactDOM.render(
   <Provider store={store}>
