a8dcbb1e104a9313f0cde8473612bb9e649514e0
Sockalingam <74052417+Sockalingam29@users.noreply.github.com>
2022-09-21 20:04:21 +0530

    update url

--- frontend/src/api/index.js
+++ frontend/src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const url = 'https://memories-mern-appln.herokuapp.com/posts/';
+const url = 'https://sockalingam-memories.herokuapp.com/posts';
 
 export const fetchPosts = () => axios.get(url);
 
