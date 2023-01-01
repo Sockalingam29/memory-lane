115bf3c2a2a8fcac4f992e286cf7be2a8b83da92
Sockalingam <74052417+Sockalingam29@users.noreply.github.com>
2023-01-01 14:26:57 +0530

    update and delete privileges only to author

--- frontend/src/components/postCards.js
+++ frontend/src/components/postCards.js
@@ -1,51 +1,52 @@
-import React from 'react';  
-import {Card,Badge} from 'react-bootstrap';
+import React from 'react';
+import { Card, Badge } from 'react-bootstrap';
 import likeIcon from '../images/like.png';
 import deleteIcon from '../images/garbage.png';
 import editIcon from '../images/editing.png';
 import '../styles/postCards.css';
-import {useNavigate} from 'react-router-dom';
-import {useDispatch} from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
 import { likePost, deletePost } from '../actions/posts';
 
-export default function postCards({currPost, setCurrentId, user}){
+export default function postCards({ currPost, setCurrentId, user }) {
     const dispatch = useDispatch();
     const navigate = useNavigate();
+    const isAuthor = user != null ? (user.result.sub === currPost.author || user.result._id === currPost.author) : false;
 
-    return(
-            <Card style={{background:"",boxShadow:"20px"}}>
-                <Card.ImgOverlay className="cardAction">
-                    <Card.Text>{currPost.authorName}</Card.Text>
-                    <div onClick={()=>setCurrentId(currPost._id)}><img src={editIcon} alt="Edit" className="actionBtn" /></div>
-                </Card.ImgOverlay>  
-                <Card.Img variant="top" src={currPost.selectedFile} alt={"Cover image"} />
-            
-                <Card.Body>
-                    <Card.Title>
-                        {currPost.title}
-                        {currPost.tags.map((tag)=>{
-                            return(
-                                <Badge style={{margin:"0 3px", fontSize:"12px"}} bg="dark">{tag}</Badge>         
-                            )
-                        })}
-                    </Card.Title>
-                    <Card.Text>
+    return (
+        <Card style={{ background: "", boxShadow: "20px" }}>
+            <Card.ImgOverlay className="cardAction">
+                <Card.Text>{currPost.authorName}</Card.Text>
+                {isAuthor && <div onClick={() => setCurrentId(currPost._id)}><img src={editIcon} alt="Edit" className="actionBtn" /></div>}
+            </Card.ImgOverlay>
+            <Card.Img variant="top" src={currPost.selectedFile} alt={"Cover image"} />
+
+            <Card.Body>
+                <Card.Title>
+                    {currPost.title}
+                    {currPost.tags.map((tag) => {
+                        return (
+                            <Badge style={{ margin: "0 3px", fontSize: "12px" }} bg="dark">{tag}</Badge>
+                        )
+                    })}
+                </Card.Title>
+                <Card.Text>
                     {currPost.content}
-                    </Card.Text>
-                    <Card.Text className='cardAction'>
-                    
+                </Card.Text>
+                <Card.Text className='cardAction'>
+
                     <div>
-                        <img className="actionBtn" alt="Like" src={likeIcon} onClick={() => user!=null ? dispatch(likePost(currPost._id)) : navigate("/auth")}/>
+                        <img className="actionBtn" alt="Like" src={likeIcon} onClick={() => user != null ? dispatch(likePost(currPost._id)) : navigate("/auth")} />
                         <small className='align-bottom'>{' '}{currPost.likes.length}</small>
                     </div>
-                    
-                    <div>
-                        <img className='actionBtn' onClick={() => dispatch(deletePost(currPost._id))} alt="Delete" src={deleteIcon}/>
-                    </div>
-                    </Card.Text>
-                </Card.Body>
-             </Card>
-        
+
+                    {isAuthor && <div>
+                        <img className='actionBtn' onClick={() => dispatch(deletePost(currPost._id))} alt="Delete" src={deleteIcon} />
+                    </div>}
+                </Card.Text>
+            </Card.Body>
+        </Card>
+
     )
 }
 
